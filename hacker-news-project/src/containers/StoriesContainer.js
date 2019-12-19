2653ad8f5f86e026d0c637b2bb7607048851083b
shawn-nyquist <shawn.nyquist@gmail.com>
2019-12-18 19:49:05 -0600

    Setup API call and display results

---
+++ hacker-news-project/src/containers/StoriesContainer.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import { getStoryIds } from "../services/hnApi";
+import { Story } from '../components/Story';
+
+export const StoriesContainer = () => {
+  const [storyIds, setStoryIds] = useState([]);
+
+  useEffect(() => {
+    //Resolves API promise
+    getStoryIds().then(data => setStoryIds(data));
+  }, []);
+
+  return storyIds.map(storyId => <Story key={storyId} storyId={storyId} />)
+};
