11e299acf4a44f14278f4ee3ecdf15f3021cb4f9
shawn-nyquist <shawn.nyquist@gmail.com>
2019-12-20 09:53:11 -0600

    Added Infinite Scroll with limited results

--- hacker-news-project/src/hooks/useInfiniteScroll.js
+++ hacker-news-project/src/hooks/useInfiniteScroll.js
@@ -1,11 +1,45 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, memo } from 'react';
 import { STORY_INCREMENT, MAX_STORIES } from '../constants';
+import { debounce } from '../utilities/debounce';
 
 export const useInfiniteScroll = () => {
-    const [loading, setLoading] = useState(false);
-    const [count, setCount] = useState(STORY_INCREMENT);
+  const [loading, setLoading] = useState(false);
+  const [count, setCount] = useState(STORY_INCREMENT);
 
-    
-}
+  const handleScroll = debounce(() => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+        document.documentElement.offsetHeight ||
+      loading
+    ) {
+      return false;
+    }
+
+    setLoading(true);
+  }, 500);
+
+  useEffect(() => {
+    // If nothing loads, it returns  
+    if (!loading) return;
+
+    // If the count with the increment number is greater than max stories it will stop at max
+    if (count + STORY_INCREMENT >= MAX_STORIES) {
+      setCount(MAX_STORIES);
+    } else {
+      setCount(count + STORY_INCREMENT);
+    }
+
+    setLoading(false);
+  }, [loading]);
+
+
+  // Scroll Listener  
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return { count };
+};
 
 
