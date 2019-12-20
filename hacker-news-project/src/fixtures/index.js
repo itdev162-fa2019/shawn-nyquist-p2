b6f6da926fe9d2e16f6f003aa9b6c534d660afeb
shawn-nyquist <shawn.nyquist@gmail.com>
2019-12-20 10:06:50 -0600

    Scroll Fixed with correct results loading

---
+++ hacker-news-project/src/fixtures/index.js
@@ -0,0 +1,17 @@
+export const singularStory = {
+    by: 'Shawn Nyquist',
+    id: 1,
+    time: 2342342343,,
+    title: 'This is an amazing practice project',
+    url: 'https://shawnnyquist.com/projectstuff',
+};
+
+export const storyIds = [1];
+
+export const emptySingleStory = {
+    by: undefined,
+    id: undefined,
+    time: undefined,
+    title: undefined,
+    url: undefined,
+};
