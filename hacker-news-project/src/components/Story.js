81acf32a7a821143a0173f156c45da0aad52817f
shawn-nyquist <shawn.nyquist@gmail.com>
2019-12-19 20:11:12 -0600

    Added Styled Components for API results

--- hacker-news-project/src/components/Story.js
+++ hacker-news-project/src/components/Story.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect } from 'react';
 import { getStory } from '../services/hnApi';
+import { StoryWrapper, StoryTitle, StoryMeta, StoryMetaElement } from '../styles/StoryStyles';
 
 
 export const Story = ({ storyId }) => {
@@ -10,12 +11,20 @@ export const Story = ({ storyId }) => {
     }, []);
 
     return story && story.url ? (
-        <>
-            <a href={story.url}>
-                <p>{story.title}</p>
-            </a>
-            By: <p>{story.by}</p>
-            Posted: <p>{story.time}</p>
-        </>
-    ) :  null;
+      <StoryWrapper data-testid="story">
+        <StoryTitle>
+          <a href={story.url}>{story.title}</a>
+        </StoryTitle>
+        <StoryMeta>
+          <span data-testid="story-by">
+            <StoryMetaElement color="#000">By: </StoryMetaElement>
+            {story.by}
+          </span>
+          <span data-testid="story-time">
+            <StoryMetaElement color="#000">Posted: </StoryMetaElement>
+            {story.time}
+          </span>
+        </StoryMeta>   
+      </StoryWrapper>
+    ) : null;
 };
--- hacker-news-project/src/services/hnApi.js
+++ hacker-news-project/src/services/hnApi.js
@@ -1,13 +1,17 @@
 import axios from 'axios';
 
+import { selectFields } from '../utilities/selectFields';
+
 export const baseUrl = 'https://hacker-news.firebaseio.com/v0/';
 export const newStoriesUrl = `${baseUrl}newstories.json`;
 export const storyUrl = `${baseUrl}item/`;
 
-export const getStory = async (storyId) => {
+export const getStory = async storyId => {
     const result = await axios
         .get(`${storyUrl + storyId}.json`)
-        .then(({data}) => data);
+        .then(({data}) => data && selectFields(data));
+
+    return result; 
 }
 
 export const getStoryIds = async () => {
